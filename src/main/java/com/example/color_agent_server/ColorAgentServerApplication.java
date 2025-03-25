4954e7ca9e9d1fdc4ab7e3de54ec01a0fb144aa3
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-03-25 20:07:51 +0900

    init

---
+++ src/main/java/com/example/color_agent_server/ColorAgentServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.color_agent_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorAgentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorAgentServerApplication.class, args);
+	}
+
+}
