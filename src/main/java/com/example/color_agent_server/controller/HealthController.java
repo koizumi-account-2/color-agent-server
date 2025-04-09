323e0779e65dc3d5a81e5a2bea644bbe0fdb6fc2
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-04-09 23:00:44 +0900

    add log

--- src/main/java/com/example/color_agent_server/controller/HealthController.java
+++ src/main/java/com/example/color_agent_server/controller/HealthController.java
@@ -2,14 +2,23 @@ package com.example.color_agent_server.controller;
 
 import com.example.color_agent_server.util.PresentationWebClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
 public class HealthController implements HealthApi {
+
+    @Value("${datasource.url}")
+    private String url;
+    @Value("${datasource.username}")
+    private String userName;
+    @Value("${datasource.password}")
+    private String password;
     @Override
     public ResponseEntity<Void> checkHealth() {
+        System.out.println(url+"-"+userName+"-"+password);
         return ResponseEntity.ok().build();
     }
 }
